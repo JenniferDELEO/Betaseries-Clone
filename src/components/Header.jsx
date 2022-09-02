bdea2fb2f83198cfbd9564bf8c40f08dc5eb5841
Jennifer DE LEO <deleo.jennifer1@gmail.com>
2022-09-02 17:09:34 +0200

    put the search in the navbar again

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink, useNavigate } from "react-router-dom";
 import { AiFillHome } from "react-icons/ai";
 import { MdComputer } from "react-icons/md";
-import { IoMdFilm } from "react-icons/io";
+import { IoMdFilm, IoMdClose } from "react-icons/io";
+import { BsSearch } from "react-icons/bs";
+import axios from "axios";
 
 const Header = () => {
   let navigate = useNavigate();
+  const [search, setSearch] = useState(false);
+  const [searchInput, setSearchInput] = useState("");
+  const [searchResultShow, setSearchResultShow] = useState([]);
+  const [searchResultMovie, setSearchResultMovie] = useState([]);
+  const [showId, setShowId] = useState("");
+  const [movieId, setMovieId] = useState("");
+
   const token = localStorage.getItem("token");
+
+  axios
+    .get(
+      `https://api.betaseries.com/search/all?key=${process.env.REACT_APP_KEY}&query=${searchInput}&limit=12`
+    )
+    .then((res) => {
+      setSearchResultShow(res.data.shows);
+      setSearchResultMovie(res.data.movies);
+    });
+
+  axios
+    .get(
+      `https://api.betaseries.com/shows/display?key=${process.env.REACT_APP_KEY}&id=${showId}`
+    )
+    .then((res) => res.data)
+    .then((data) => {});
 
   const handleLogOut = () => {
     localStorage.setItem("token", "");
@@ -17,83 +42,150 @@ const Header = () => {
     <div className="header">
       <nav>
         {token ? (
-          <div className="navbar">
-            <ul>
+          search ? (
+            <div className="searchContainer">
               <div className="title">
                 <NavLink to="/">
                   <li>betaseries</li>
                 </NavLink>
               </div>
-              <div className="navbar">
-                <NavLink
-                  to="/"
-                  className={(nav) =>
-                    nav.isActive ? "nav-active" : "nav-inactive"
-                  }
-                >
-                  <li>
-                    <AiFillHome
-                      size={20}
-                      style={{
-                        width: "40px",
-                      }}
-                    />
-                    <p>Accueil</p>
-                  </li>
-                </NavLink>
-                <div className="navbar-dropdown">
-                  <NavLink
-                    to="/shows"
-                    className={(nav) =>
-                      nav.isActive ? "nav-active" : "nav-inactive"
-                    }
-                  >
-                    <li>
-                      <MdComputer
-                        size={20}
-                        style={{
-                          width: "40px",
-                        }}
-                      />
-                      <p>Séries</p>
-                    </li>
-                  </NavLink>
-                  <div className="dropdown-content">
-                    <NavLink to="/shows">Toutes les séries</NavLink>
-                    <NavLink to="/my-shows">Mes séries</NavLink>
-                    <NavLink to="/episodes">Episodes à voir</NavLink>
-                    <NavLink to="/planning">Planning des sorties</NavLink>
-                  </div>
+              <div className="searchSection">
+                <div>
+                  <BsSearch size={20} />
                 </div>
-                <div className="navbar-dropdown">
-                  <NavLink
-                    to="/movies"
-                    className={(nav) =>
-                      nav.isActive ? "nav-active" : "nav-inactive"
-                    }
-                  >
-                    <li>
-                      <IoMdFilm
-                        size={20}
-                        style={{
-                          width: "40px",
-                        }}
-                      />
-                      <p>Films</p>
-                    </li>
-                  </NavLink>
-                  <div className="dropdown-content">
-                    <NavLink to="/movies">Tous les films</NavLink>
-                    <NavLink to="/my-movies">Mes films</NavLink>
-                  </div>
+                <form>
+                  <input
+                    className="searchInput"
+                    type="text"
+                    placeholder="Rechercher une série, un film"
+                    value={searchInput}
+                    onChange={(e) => setSearchInput(e.target.value)}
+                  />
+                </form>
+                <div>
+                  <IoMdClose
+                    size={20}
+                    onClick={() => setSearch(!search)}
+                    style={{ cursor: "pointer" }}
+                  />
                 </div>
               </div>
-            </ul>
-            <div className="connection">
-              <NavLink to="/profil">Profil</NavLink>
-              <NavLink to="/" onClick={() => handleLogOut()}>
-                Se déconnecter
+            </div>
+          ) : (
+            <div className="navbar">
+              <ul>
+                <div className="title">
+                  <NavLink to="/">
+                    <li>betaseries</li>
+                  </NavLink>
+                </div>
+                <div className="navbar">
+                  <NavLink
+                    to="/"
+                    className={(nav) =>
+                      nav.isActive ? "nav-active" : "nav-inactive"
+                    }
+                  >
+                    <li>
+                      <AiFillHome
+                        size={20}
+                        style={{
+                          width: "40px",
+                        }}
+                      />
+                      <p>Accueil</p>
+                    </li>
+                  </NavLink>
+                  <div className="navbar-dropdown">
+                    <NavLink
+                      to="/shows"
+                      className={(nav) =>
+                        nav.isActive ? "nav-active" : "nav-inactive"
+                      }
+                    >
+                      <li>
+                        <MdComputer
+                          size={20}
+                          style={{
+                            width: "40px",
+                          }}
+                        />
+                        <p>Séries</p>
+                      </li>
+                    </NavLink>
+                    <div className="dropdown-content">
+                      <NavLink to="/shows">Toutes les séries</NavLink>
+                      <NavLink to="/my-shows">Mes séries</NavLink>
+                      <NavLink to="/episodes">Episodes à voir</NavLink>
+                      <NavLink to="/planning">Planning des sorties</NavLink>
+                    </div>
+                  </div>
+                  <div className="navbar-dropdown">
+                    <NavLink
+                      to="/movies"
+                      className={(nav) =>
+                        nav.isActive ? "nav-active" : "nav-inactive"
+                      }
+                    >
+                      <li>
+                        <IoMdFilm
+                          size={20}
+                          style={{
+                            width: "40px",
+                          }}
+                        />
+                        <p>Films</p>
+                      </li>
+                    </NavLink>
+                    <div className="dropdown-content">
+                      <NavLink to="/movies">Tous les films</NavLink>
+                      <NavLink to="/my-movies">Mes films</NavLink>
+                    </div>
+                  </div>
+                </div>
+              </ul>
+              <div className="connection">
+                <NavLink to="/profil">Profil</NavLink>
+                <NavLink to="/" onClick={() => handleLogOut()}>
+                  Se déconnecter
+                </NavLink>
+                <div className="searchIcon">
+                  <BsSearch
+                    size={20}
+                    onClick={() => setSearch(!search)}
+                    style={{ cursor: "pointer" }}
+                  />
+                </div>
+              </div>
+            </div>
+          )
+        ) : search ? (
+          <div className="searchContainer">
+            <div className="title">
+              <NavLink to="/">
+                <li>betaseries</li>
               </NavLink>
+            </div>
+            <div className="searchSection">
+              <div>
+                <BsSearch size={20} />
+              </div>
+              <form>
+                <input
+                  className="searchInput"
+                  type="text"
+                  placeholder="Rechercher une série, un film"
+                  value={searchInput}
+                  onChange={(e) => setSearchInput(e.target.value)}
+                />
+              </form>
+              <div>
+                <IoMdClose
+                  size={20}
+                  onClick={() => setSearch(!search)}
+                  style={{ cursor: "pointer" }}
+                />
+              </div>
             </div>
           </div>
         ) : (
@@ -172,10 +264,31 @@ const Header = () => {
             <div className="connection">
               <NavLink to="/login">Connexion</NavLink>
               <NavLink to="/signup">Inscription</NavLink>
+              <div className="searchIcon">
+                <BsSearch
+                  size={20}
+                  onClick={() => setSearch(!search)}
+                  style={{ cursor: "pointer" }}
+                />
+              </div>
             </div>
           </div>
         )}
       </nav>
+      {searchInput.length !== 0 ? (
+        <div className="searchResultsContainer">
+          <div className="searchResultsInner">
+            {/* {searchResultShow.map((show) => (
+            <SearchShows key={show.id} show={show} />
+          ))}
+          {searchResultMovie.map((movie) => (
+            <SearchMovies key={movie.id} movie={movie} />
+          ))} */}
+          </div>
+        </div>
+      ) : (
+        ""
+      )}
     </div>
   );
 };
