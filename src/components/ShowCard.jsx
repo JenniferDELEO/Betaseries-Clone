b9723f8225acdd4750debee817a6097164afa89d
Jennifer DE LEO <deleo.jennifer1@gmail.com>
2022-09-06 19:23:24 +0200

    started style on the show cards

--- src/components/ShowCard.jsx
+++ src/components/ShowCard.jsx
@@ -1,19 +1,39 @@
 import React from "react";
 
-const Card = ({ show }) => {
+const ShowCard = ({ show }) => {
   return (
-    <div>
+    <div className="resultContainer">
       <img
-        src={
-          show.images.poster
-            ? show.images.poster
-            : "../assets/images/pas_dimage.webp"
-        }
-        alt="poster"
+        src={show.images.poster ? show.images.poster : "images/pas-dimage.webp"}
+        alt="poster série"
       />
-      <p>{show.title}</p>
+      <div>
+        <h2>
+          {show.title.length > 20
+            ? show.title.slice(0, 20) + "..."
+            : show.title}
+        </h2>
+        {Object.keys(show.genres)
+          ? Object.keys(show.genres)
+              .slice(0, 4)
+              .map((key, index) => (
+                <p key={index} value={key}>
+                  {show.genres[key]}
+                </p>
+              ))
+          : ""}
+        <p>
+          Plateforme :{" "}
+          {/* {show.platforms.svods[0] !== null
+            ? show.platforms.svods[0].name
+            : show.platforms.svod !== null
+            ? show.platforms.svod.name
+            : ""} */}
+        </p>
+        <p>{show.creation}</p>
+      </div>
     </div>
   );
 };
 
-export default Card;
+export default ShowCard;
