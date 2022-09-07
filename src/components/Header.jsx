b0c4d88406ae0dda65a36e9fc673df8d981bf1f3
Jennifer DE LEO <deleo.jennifer1@gmail.com>
2022-09-07 16:30:11 +0200

    finished style show page + header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -17,6 +17,40 @@ const Header = () => {
   const [searchResultMovie, setSearchResultMovie] = useState([]);
   const [nbpp, setNbpp] = useState(5);
   const ref = useRef(null);
+  const safeDocument = typeof document !== "undefined" ? document : {};
+  const scrollBlocked = useRef();
+  const html = safeDocument.documentElement;
+  const { body } = safeDocument;
+
+  const blockScroll = () => {
+    if (!body || !body.style || scrollBlocked.current) return;
+
+    const scrollBarWidth = window.innerWidth - html.clientWidth;
+    const bodyPaddingRight =
+      parseInt(
+        window.getComputedStyle(body).getPropertyValue("padding-right")
+      ) || 0;
+
+    html.style.position = "relative"; /* [1] */
+    html.style.overflow = "hidden"; /* [2] */
+    body.style.position = "relative"; /* [1] */
+    body.style.overflow = "hidden"; /* [2] */
+    body.style.paddingRight = `${bodyPaddingRight + scrollBarWidth}px`;
+
+    scrollBlocked.current = true;
+  };
+
+  const allowScroll = () => {
+    if (!body || !body.style || !scrollBlocked.current) return;
+
+    html.style.position = "";
+    html.style.overflow = "";
+    body.style.position = "";
+    body.style.overflow = "";
+    body.style.paddingRight = "";
+
+    scrollBlocked.current = false;
+  };
 
   const token = localStorage.getItem("token");
   let config = {
@@ -51,11 +85,13 @@ const Header = () => {
   async function handleFocusOnSearch() {
     await setSearch(!search);
     ref.current.focus();
+    blockScroll();
   }
 
   const handleSearchClose = () => {
     setSearch(!search);
     setSearchInput("");
+    allowScroll();
   };
 
   return (
